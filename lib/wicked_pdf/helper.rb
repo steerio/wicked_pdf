19bdb0312991c3d4a3b475ee85037243ca1b287b
Romain <romain.laurent@wopata.com>
2011-12-12 14:13:32 +0100

    Fix Rails 3.1 assets, Keep images in public folder

--- lib/wicked_pdf/helper.rb
+++ lib/wicked_pdf/helper.rb
@@ -1,22 +1,26 @@
 module WickedPdf
   module Helper
-    def wicked_pdf_stylesheet_link_tag(*sources)
-      css_dir = Rails.root.join('public','stylesheets')
+    def wicked_pdf_stylesheet_link_tag *sources
       sources.collect { |source|
-        "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
-      }.join("\n").html_safe
+        "<style type='text/css'>#{Rails.application.assets.find_asset("#{source}.css").body}</style>"
+      }.join("\n").gsub(/url\(['"]?(.+)['"]?\)(.+)/,%[url("#{wicked_pdf_image_location("\\1")}")\\2]).html_safe
     end
 
-    def wicked_pdf_image_tag(img, options={})
-      image_tag "file://#{Rails.root.join('public', 'images', img)}", options
+    def wicked_pdf_image_tag img, options={}
+      image_tag wicked_pdf_image_location(img), options
     end
 
-    def wicked_pdf_javascript_src_tag(jsfile, options={})
-      javascript_src_tag "file://#{Rails.root.join('public','javascripts',jsfile)}", options
+    def wicked_pdf_javascript_src_tag jsfile
+      "<script type='text/javascript'>#{Rails.application.assets.find_asset("#{source}.js").body}</script>"
     end
 
-    def wicked_pdf_javascript_include_tag(*sources)
+    def wicked_pdf_javascript_include_tag *sources
       sources.collect{ |source| wicked_pdf_javascript_src_tag(source, {}) }.join("\n").html_safe
     end
+
+    def wicked_pdf_image_location img
+      "file://#{Rails.root.join('public', img)}"
+    end
+
   end
 end
